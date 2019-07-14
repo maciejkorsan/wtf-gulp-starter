37720f8e26c4d97117a7633974ee105d48088c7c
Maciej Korsan <maciejkorsan@gmail.com>
2019-07-14 13:27:51 +0200

    Updates

--- serviceworker.js
+++ serviceworker.js
@@ -17,12 +17,8 @@
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
-// A list of local resources we always want to be cached.
+// A list of local resources we always want to be cached. IE. 'index.html', 'css/main.css'
 const PRECACHE_URLS = [
-  'index.html',
-  './',
-  'css/main.css',
-  'js/main.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
@@ -49,26 +45,16 @@ self.addEventListener('activate', event => {
 });
 
 // The fetch handler serves responses for same-origin resources from a cache.
-// If no response is found, it populates the runtime cache with the response
-// from the network before returning it to the page.
+// If no response is found, it responses with the original request
 self.addEventListener('fetch', event => {
-  // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
         if (cachedResponse) {
           return cachedResponse;
-        }
-
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
-            // Put a copy of the response in the runtime cache.
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
-          });
-        });
-      })
-    );
+        } else
+        return fetch(event.request);
+      }) 
+    )
   }
 });
