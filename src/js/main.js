1100c6c0f7bf6d708cf3afd76300ee36e11d4816
Kasia Izak <kontakt@kasiaizak.pl>
2019-11-12 16:03:00 +0100

    change configuration

--- src/js/main.js
+++ src/js/main.js
@@ -15,4 +15,15 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
-console.log(`Hello world!`);
+import browserUpdate from 'browser-update';
+browserUpdate({
+  required: {e:-2,f:-6,o:-6,s:10,c:-6},
+  insecure:true,
+  unsupported:true,
+  reminder:0,
+  reminderClosed: 1
+});
+
+const autor = 'Kasia Izak';
+console.log(`Hej, nazywam się ${autor} i jestem autorką tej strony.
+Fajnie, że nie boisz się zaglądać do konsoli! ;-)`);
